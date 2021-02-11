232bb11eb45c49d46cc2c50c37fbe488c841fadb
pawan <pawanbhatta178@gmail.com>
2021-02-10 21:17:45 -0500

    overlay

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -7,20 +7,33 @@ import { CgClose } from "react-icons/cg";
 const SideMenu = ({ show, onClose }) => {
   useDisableBodyScroll(show);
   return (
-    <CSSTransition in={show} timeout={400} classNames="side-menu" unmountOnExit>
-      <div className="absolute top-0 shadow-lg">
-        <div className=" w-64 bg-primary overflow-y-hidden h-screen z-40 px-6 py-4">
-          <div className="flex">
-            <div className="hover-transition rounded-full p-1">
-              <CgClose
-                className="text-3xl font-semibold cursor-pointer "
-                onClick={onClose}
-              />
+    <>
+      <CSSTransition
+        in={show}
+        timeout={400}
+        classNames="side-menu"
+        unmountOnExit
+      >
+        <div className="absolute top-0 shadow-lg z-20">
+          <div className=" w-64 bg-primary overflow-y-hidden h-screen  px-6 py-4">
+            <div className="flex">
+              <div className="hover-transition rounded-full p-1">
+                <CgClose
+                  className="text-3xl font-semibold cursor-pointer "
+                  onClick={onClose}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </CSSTransition>
+      </CSSTransition>
+      <CSSTransition in={show} timeout={400} classNames="overlay" unmountOnExit>
+        <div
+          className="absolute top-0 h-screen w-screen z-10 bg-overlay"
+          onClick={onClose}
+        ></div>
+      </CSSTransition>
+    </>
   );
 };
 
