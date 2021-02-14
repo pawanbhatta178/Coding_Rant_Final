e27a705a5732e11d283789e6a08a89d43dbf9c52
pawan <pawanbhatta178@gmail.com>
2021-02-14 18:57:54 -0500

    Workspace started

--- src/components/NavBar/useNavBar.js
+++ src/components/NavBar/useNavBar.js
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext } from "react";
 import { UserContext } from "../../UserContext";
 
 const useNavBar = () => {
--- src/pages/WorkSpace/WorkSpace.js
+++ src/pages/WorkSpace/WorkSpace.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./WorkSpace.css";
+import Split from "react-split";
 
 const WorkSpace = () => {
-  return <div>Workspace</div>;
+  return (
+    <div className="w-screen h-screen border border-purple-500 justify-center items-center">
+      <Split className="w-full h-full flex" sizes={[25, 75]}>
+        <div className="code-editor border ">Code-editor</div>
+        <div className="question border">Question</div>
+      </Split>
+    </div>
+  );
 };
 
 export default WorkSpace;
