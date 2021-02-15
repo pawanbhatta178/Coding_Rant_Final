5bb40ece2025e72e5f7915afbb69fa0c56859268
pawan <pawanbhatta178@gmail.com>
2021-02-14 22:24:06 -0500

    responsive split

---
+++ src/components/common/useLocalStorage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const useLocalStorage = () => {
+  return <div></div>;
+};
+
+export default useLocalStorage;
--- src/pages/WorkSpace/WorkSpace.js
+++ src/pages/WorkSpace/WorkSpace.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WorkSpace.css";
 import SplitPane from "react-split-pane";
+import useWindowDimensions from "../../components/common/useWindowDimensions";
 const WorkSpace = () => {
+  const { width } = useWindowDimensions();
+
+  const resolveMinWidth = (viewWidth) => {
+    if (viewWidth < 576) {
+      return 200;
+    }
+    if (viewWidth < 768) {
+      return 200;
+    }
+    if (viewWidth < 992) {
+      return 250;
+    }
+    return 290;
+  };
+
+  const resolveMaxWidth = (viewWidth) => {
+    if (viewWidth < 576) {
+      return 300;
+    }
+    if (viewWidth < 768) {
+      return 450;
+    }
+    if (viewWidth < 992) {
+      return 690;
+    }
+    return 900;
+  };
+
   return (
     <div className="w-screen h-screen flex justify-center items-center">
+      {console.log(width)}
       <SplitPane
         split="vertical"
-        minSize={50}
+        minSize={resolveMinWidth(width)}
+        maxSize={resolveMaxWidth(width)}
         defaultSize={parseInt(localStorage.getItem("splitPos"), 10)}
         onChange={(size) => localStorage.setItem("splitPos", size)}
       >
