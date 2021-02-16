e486ef984aa212861b9a67f0a33ff6642d99d4b9
pawan <pawanbhatta178@gmail.com>
2021-02-16 12:33:16 -0500

    padding fixes

--- src/components/SplitScreen/RightPane.js
+++ src/components/SplitScreen/RightPane.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const RightPane = ({ children }) => {
-  return <div className="h-full shadow-lg px-2">{children}</div>;
+  return <div className="h-full shadow-lg">{children}</div>;
 };
 
 export default RightPane;
