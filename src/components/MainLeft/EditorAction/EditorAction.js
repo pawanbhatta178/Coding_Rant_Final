d71fe4e93f619a9317aacbc1b044cd04e1608ded
pawan <pawanbhatta178@gmail.com>
2021-02-21 16:40:29 -0500

    Submission starting

--- src/components/MainLeft/EditorAction/EditorAction.js
+++ src/components/MainLeft/EditorAction/EditorAction.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const EditorAction = () => {
-  return <div className="h-full border">EditorAction</div>;
+  return (
+    <div className="h-full border-t mb-8">
+      <button onClick={() => console.log("Submitting")}>Submit</button>
+    </div>
+  );
 };
 
 export default EditorAction;
