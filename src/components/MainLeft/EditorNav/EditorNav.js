9befaa1bfc07f44013e950a88c811fcc109f330e
pawan <pawanbhatta178@gmail.com>
2021-02-21 16:16:07 -0500

    Select Component

--- src/components/MainLeft/EditorNav/EditorNav.js
+++ src/components/MainLeft/EditorNav/EditorNav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useEditorNav from "./useEditorNav";
-
+import { RiMoonFill, RiSunFill } from "react-icons/ri";
+import "./EditorNav.css";
 const EditorNav = ({
   theme,
   toggleTheme,
@@ -12,27 +13,31 @@ const EditorNav = ({
     changeActiveLanguage
   );
   return (
-    <>
-      {console.log(activeLanguage, languageOptions)}
-      <div className="flex bg-primary">
-        <button onClick={toggleTheme}>{theme}</button>
+    <div className="flex justify-end gap-x-4 py-2 items-stretch h-11">
+      <div
+        onClick={toggleTheme}
+        className="px-2 bg-secondary shadow-lg border border-accent rounded-lg cursor-pointer"
+      >
+        <div className="w-full h-full flex items-center ">
+          {theme === "vs-dark" ? (
+            <RiMoonFill className="text-xl " />
+          ) : (
+            <RiSunFill className="text-xl " />
+          )}
+        </div>
       </div>
-      <select id="language" onChange={handleSelectChange}>
+      <select
+        className="shadow-lg border border-accent rounded-lg cursor-pointer focus:outline-none bg-secondary "
+        id="language"
+        onChange={handleSelectChange}
+      >
         {languageOptions.map((option) => (
           <option value={option.value} selected={option.selected}>
             {option.name}
           </option>
         ))}
-        {/* <option value="javascript">JavaScript</option>
-        <option value="c++" disabled>
-          C++
-        </option>
-        <option value="java" disabled>
-          Java
-        </option>
-        <option value="typescript">TypeScript</option> */}
       </select>
-    </>
+    </div>
   );
 };
 
