83240d9c1016c94079cbe65134555f9a0c05abf5
pawan <pawanbhatta178@gmail.com>
2021-02-25 16:28:53 -0500

    bug: componenet loading before the authorization took place

--- src/pages/WorkSpace/useWorkspace.js
+++ src/pages/WorkSpace/useWorkspace.js
@@ -1,11 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useDisableBodyScroll } from "../../components/common/useDisableBodyScroll";
 import useScrollToTop from "../../components/common/useScrollToTop";
 import { useMutation } from "react-query";
 import { compile } from "../../api/Compile";
 import useLocalStorage from "../../components/common/useLocalStorage";
-import { getAllSubmissions } from "../../api/Submission";
-import { StringToJson } from "../../components/common/StringToJson";
 
 const langId = {
   javascript: "js",
@@ -19,7 +17,6 @@ const useWorkspace = () => {
   const [isCodeSubmitting, setCodeSubmitting] = useState(false);
   const [activeQuestionId, setActiveQuestionId] = useState("1");
   const [latestSubmission, setLatestSubmission] = useState();
-  const [submissions, setSubmissions] = useState();
   const [activeLanguage, setActiveLanguage] = useLocalStorage(
     "activeLanguage",
     "javascript"
@@ -29,22 +26,6 @@ const useWorkspace = () => {
     `Code:${activeQuestionId}:${activeLanguage}`,
     ""
   );
-
-  // const { data: submissions } = useQuery(
-  //   ["submissions", { activeQuestionId }],
-  //   () => getAllSubmissions({ questionId: activeQuestionId })
-  // );
-
-  useEffect(() => {
-    console.log("running");
-    (async () => {
-      const data = await getAllSubmissions({ questionId: activeQuestionId });
-      const formattedData = StringToJson(data, "testResult");
-      setSubmissions((_) => {
-        return formattedData;
-      });
-    })();
-  }, [activeQuestionId]);
 
   const changeCode = (newCode, _) => {
     setCode(newCode);
@@ -89,7 +70,6 @@ const useWorkspace = () => {
     changeCode,
     activeLanguage,
     changeActiveLanguage,
-    submissions,
     latestSubmission,
   };
 };
