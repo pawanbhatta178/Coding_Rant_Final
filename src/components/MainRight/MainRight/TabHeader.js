c02a228743cf035edeac1c76ded345bd9ac1b330
pawan <pawanbhatta178@gmail.com>
2021-03-13 21:34:50 -0500

    icons added

--- src/components/MainRight/MainRight/TabHeader.js
+++ src/components/MainRight/MainRight/TabHeader.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { BiMessageAltDetail, BiTrophy } from "react-icons/bi";
+import { FiUploadCloud } from "react-icons/fi";
 
-const baseStyle = "button-base-style font-thin  px-2 h-7";
+const baseStyle = "button-base-style font-thin  px-2 h-7 flex items-center";
 
 const selectedStyle = `${baseStyle} border-b-2 border-accent font-light`;
 
@@ -11,6 +13,10 @@ const TabHeader = ({ name, id, handleTabClick, selected }) => {
       value={id}
       onClick={handleTabClick}
     >
+      {name === "Description" && <BiMessageAltDetail />}
+      {name === "Submissions" && <FiUploadCloud />}
+      {name === "LeaderBoard" && <BiTrophy />}
+      {console.log(id)}
       {name}
     </button>
   );
