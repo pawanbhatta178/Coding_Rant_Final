979673015f97d951c2edd1cff280eed1369ace75
pawan <pawanbhatta178@gmail.com>
2021-03-13 20:23:17 -0500

    styled sample input

--- src/components/MainRight/Description/CardInput.js
+++ src/components/MainRight/Description/CardInput.js
@@ -1,12 +1,14 @@
 import React from "react";
+import "./CardInput.css";
 
 const CardInput = ({ title, children, ...props }) => {
   return (
     <div {...props}>
       <div className="font-medium">{title}</div>
-      <div className=" text-secondary bg-primary py-2 px-2 mt-1 rounded-md overflow-y-scroll max-h-20 shadow-sm font-light">
-        {children}
-      </div>
+      <div
+        className="bg-secondary-shade py-2 px-2 mt-1 rounded-md overflow-y-scroll max-h-20 shadow-sm font-light"
+        dangerouslySetInnerHTML={{ __html: children }}
+      ></div>
     </div>
   );
 };
