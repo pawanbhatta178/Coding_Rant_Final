4ddb4c3abed8f30eb2b5ec09398ad7ee62da30f4
pawan <pawanbhatta178@gmail.com>
2021-04-07 22:20:58 -0400

    compile and general services integrated

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,4 +1,4 @@
 const LOGIN_SERVICE_URI = `https://api.codingrant.com/auth`;
-const COMPILE_SERVICE_URI = `http://localhost:9999`;
-const GENERAL_SERVICE_URI = `http://localhost:9998`;
+const COMPILE_SERVICE_URI = `https://api.codingrant.com/compile`;
+const GENERAL_SERVICE_URI = `https://api.codingrant.com/`;
 export { LOGIN_SERVICE_URI, COMPILE_SERVICE_URI, GENERAL_SERVICE_URI };
