e9afa10613f506bab8b06f23cf171218bd0dcf5c
pawan <pawanbhatta178@gmail.com>
2021-04-07 22:25:14 -0400

    extra slash removed

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,4 +1,4 @@
 const LOGIN_SERVICE_URI = `https://api.codingrant.com/auth`;
 const COMPILE_SERVICE_URI = `https://api.codingrant.com/compile`;
-const GENERAL_SERVICE_URI = `https://api.codingrant.com/`;
+const GENERAL_SERVICE_URI = `https://api.codingrant.com`;
 export { LOGIN_SERVICE_URI, COMPILE_SERVICE_URI, GENERAL_SERVICE_URI };
