05195fc7a344be6454935dbf116081e7eb0e6ff3
pawan <pawanbhatta178@gmail.com>
2021-04-07 22:12:39 -0400

    Second attempt authentication service integration

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,4 +1,4 @@
-const LOGIN_SERVICE_URI = `http://104.197.4.59:3000`;
+const LOGIN_SERVICE_URI = `https://api.codingrant.com/auth`;
 const COMPILE_SERVICE_URI = `http://localhost:9999`;
 const GENERAL_SERVICE_URI = `http://localhost:9998`;
 export { LOGIN_SERVICE_URI, COMPILE_SERVICE_URI, GENERAL_SERVICE_URI };
